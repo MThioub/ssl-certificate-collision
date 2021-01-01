ddc52023d7dff6867172bc8a2d578873b7a4f52a
momo-cryto <thioubmouhamadou@gmail.com>
2021-01-01 19:56:02 +0100

    build project file

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+source .zshrc
+pyenv install 3.9.0
+pyenv global 3.9.0
+pip install --upgrade pip
+pip install pipenv
